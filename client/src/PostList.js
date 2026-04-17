1bf8952d593412fc9e02ade51fe61d4bb8fecddb
Kaspar <kylgkorvigajava@gmail.com>
2026-04-17 12:55:13 +0300

    fixed wrong port in PostList.js

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -10,7 +10,7 @@ const PostList = () => {
     useEffect(() => {
         const fetchPosts = async () => {
             try {
-                const res = await axios.get("http://localhost:5002/posts");
+                const res = await axios.get("http://localhost:5000/posts");
                 const posts = res.data;
                 setPosts(posts);
             } catch (err) {
